4db31e456e4344818271c1cc1d06b75d39b34fa4
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2024-11-11 16:35:43 +0100

    Update test_grammar_checker.py

--- tests/test_grammar_checker.py
+++ tests/test_grammar_checker.py
@@ -1,13 +1,10 @@
 import sys
 import os
-
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Paths relative to the train.py file's location
 sentences_file = os.path.join(os.path.dirname(__file__), '../data/test_sentences.txt')
 labels_file = os.path.join(os.path.dirname(__file__), '../data/labels.txt')
 models_path = os.path.join(os.path.dirname(__file__), '../models/')
-model_path = os.path.join(models_path, 'grammar_checker_model.pth')  # Full model path with filename
+model_path = os.path.join(models_path, 'grammar_checker_model.pth') 
 
 # Try importing from the src package
 try:
@@ -25,14 +22,9 @@ def load_test_sentences(file_path=sentences_file):
     return sentences
 
 def load_and_test_model(model_path=model_path):
-    # Load the model and the vocabulary
-    model, word_to_idx = load_model_and_vocab(model_path)
-
-    # Define idx_to_label for prediction output
-    idx_to_label = {0: 'incorrect', 1: 'correct'}
-
-    # Load test sentences from the file
-    test_sentences = load_test_sentences(sentences_file)
+    model, word_to_idx = load_model_and_vocab(model_path) # Load the model and the vocabulary
+    idx_to_label = {0: 'incorrect', 1: 'correct'} # Define idx_to_label for prediction outpuT
+    test_sentences = load_test_sentences(sentences_file) # Load test sentences from the file
 
     # Test the model with the sentences
     for sentence in test_sentences:
@@ -40,8 +32,6 @@ def load_and_test_model(model_path=model_path):
         prediction = predict(model, sentence, word_to_idx, idx_to_label)
         print(f"Prediction: {prediction}")
         print()
-
-
 
 if __name__ == "__main__":
     load_and_test_model(model_path)
